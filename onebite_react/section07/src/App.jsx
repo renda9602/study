fc75f5a6f377902b20690057af039212bca84fc3
babeloper <renda9601@gmail.com>
2025-02-10 15:13:54 +0900

    useEffect_even mount, unMount 사용방법

---
+++ onebite_react/section07/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+import Viewer from './commonents/Viewer';
+import Controller from './commonents/Controller';
+import { useState, useEffect, useRef } from 'react';
+import Even from './commonents/Even';
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const isMount = useRef(false);
+
+  // 1.마운트 : 타생
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+
+    if(!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("업데이트");
+  });
+  // 3. 언마운트 : 죽음
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <input type="text" value={input} onChange={(e)=>{
+          setInput(e.target.value);
+        }} />
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null  }
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  )
+}
+
+export default App
