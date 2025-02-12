d2e7a20f39f54707065649eb751af00d222acd2c
babeloper <renda9601@gmail.com>
2025-02-12 14:01:06 +0900

    todoList3_checkbox_delete 체크박스 컨트롤, 삭제버튼 기능 추가 / 완료

--- onebite_react/section08/src/App.jsx
+++ onebite_react/section08/src/App.jsx
@@ -1,28 +1,28 @@
-import './App.css';
-import { useState, useRef } from 'react';
-import Editer from './Components/Editor';
-import Header from './Components/Header';
-import List from './Components/List';
+import { useRef, useState } from "react";
+import "./App.css";
+import Editer from "./Components/Editor";
+import Header from "./Components/Header";
+import List from "./Components/List";
 
 // 임시데이터
 const mocDate = [
   {
-    id : 0,
+    id: 0,
     isDone: false,
     content: "React 공부하기",
-    date : new Date().getTime(),
+    date: new Date().getTime(),
   },
   {
-    id : 1,
+    id: 1,
     isDone: false,
     content: "빨래하기",
-    date : new Date().getTime(),
+    date: new Date().getTime(),
   },
   {
-    id : 2,
+    id: 2,
     isDone: false,
     content: "노래 연습하기",
-    date : new Date().getTime(),
+    date: new Date().getTime(),
   },
 ];
 
@@ -31,7 +31,7 @@ function App() {
   const idRef = useRef(3);
 
   // App 안에서 뿌려줄 데이터를 새로 생성한다.
-  const onCreat = (content) => {
+  const onCreat = content => {
     const newTodo = {
       id: idRef.current++, //id값을 증가하도록 한다.
       isDone: false,
@@ -40,15 +40,40 @@ function App() {
     };
 
     setTodos([newTodo, ...todos]);
-  }
+  };
+
+  const onUpdate = targetId => {
+    // 투두 State의 값들 중에
+    // targetId의 일치하는 id를 갖는 투두 아이템의 isdDone 을 변경
+    // 인수:  todos  배열에서 targeId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    // setTodos(todos.map((todo) => {
+    //   if(todo.id === targetId) {
+    //     return {
+    //       ...todo,
+    //       isDon: !todo.isDone,
+    //     }
+    //   }
+
+    //   return todo
+    // }))
+    setTodos(
+      todos.map(todo =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = targetId => {
+    setTodos(todos.filter(todo => todo.id !== targetId));
+  };
 
   return (
     <div className="App">
       <Header />
       <Editer onCreat={onCreat} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- onebite_react/section08/src/Components/TodoItem.jsx
+++ onebite_react/section08/src/Components/TodoItem.jsx
@@ -1,14 +1,27 @@
-import "./TodoItem.css"
+import "./TodoItem.css";
 
-const TodoItem = ({id, isDone, content, date}) => {
-    return (
-        <div className="todoItem">
-            <input type="checkbox" readOnly checked={isDone} />
-            <div className="content">{content}</div>
-            <div className="date">{new Date(date).toLocaleDateString()}</div>
-            <button>삭제</button>
-        </div>
-    )
-}
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="todoItem">
+      <input
+        onChange={onChangeCheckBox}
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
 
 export default TodoItem;
