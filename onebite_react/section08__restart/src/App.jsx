d2fd9e9956fe8a6bcdef6012fbc05fd18f078c06
babeloper <renda9601@gmail.com>
2025-02-17 15:17:29 +0900

    todoList-삭제버튼 기능 추가

--- onebite_react/section08__restart/src/App.jsx
+++ onebite_react/section08__restart/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState, useRef } from 'react';
-import './App.css';
-import Header from './commonents/Header';
-import Editor from './commonents/Editor';
-import List from './commonents/List';
+import { useRef, useState } from "react";
+import "./App.css";
+import Editor from "./commonents/Editor";
+import Header from "./commonents/Header";
+import List from "./commonents/List";
 
 //임시데이터
 const mocData = [
   {
     id: 0,
     isDone: false,
-    content: 'React 공부하기',
+    content: "React 공부하기",
     date: new Date().getTime(),
   },
   {
     id: 1,
     isDone: false,
-    content: '빨래하기',
+    content: "빨래하기",
     date: new Date().getTime(),
   },
   {
     id: 2,
     isDone: false,
-    content: '노래연습하기',
+    content: "노래연습하기",
     date: new Date().getTime(),
   },
 ];
@@ -31,7 +31,7 @@ function App() {
   const idRef = useRef(3);
 
   // <인풋에 입력된 내용을 보내는 방법
-  const onCreate = (content) => {
+  const onCreate = content => {
     const newTodo = {
       // 고유 id 를 생성하기 위해 useRef 를 선언하고
       // 새로운 idRef 를 만들어서, 입력할때 생성되는 id 에 부과한다.
@@ -45,23 +45,56 @@ function App() {
   };
   // 인풋에 입력된 내용을 보내는 방법>
 
-  // 체크박스 선택 해제 보내는 방법>
-  const onUpdate = (targetId) => {
-    // todos state 값들중에 targetId 과 일치된느 아이템의 isDone 변경
-    // 인수: todos 배열에서 targetId 의 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열이 필요
+  // 모든 항목이 선택되었는지 확인
+  const isAllCompleted = todos.length > 0 && todos.every(todo => todo.isDone);
+
+  // 전체 선택/해제 함수
+  const onUpdateAll = () => {
     setTodos(
-      todos.map((todo) =>
+      todos.map(todo => ({
+        ...todo,
+        isDone: !isAllCompleted, // 현재 상태의 반대로 모든 항목 변경
+      }))
+    );
+  };
+
+  // <체크박스 선택 해제 보내는 방법
+  const onUpdate = targetId => {
+    // - todos state 값들중에 targetId 과 일치된느 아이템의 isDone 변경
+    // - 인수: todos 배열에서 targetId 의 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열이 필요
+    setTodos(
+      todos.map(todo =>
         todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
       )
     );
   };
   // 체크박스 선택 해제 보내는 방법>
 
+  // <삭제하기 버튼 설정 방법
+  const onDelete = targetId => {
+    // 인수 : totos 배열에서 targetId 와 일치하는 id를 갖는 요소만!! 삭제하는 새로운 배열이 필요.
+    // alert("삭제할까요?");
+    if (confirm("삭제할까요?") === true) {
+      document.removefrm.submit();
+    } else {
+      return false;
+    }
+
+    setTodos(todos.filter(todo => todo.id !== targetId));
+  };
+  // 삭제하기 버튼 설정 방법>
+
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} />
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onUpdateAll={onUpdateAll}
+        isAllCompleted={isAllCompleted}
+        onDelete={onDelete}
+      />
     </div>
   );
 }
--- onebite_react/section08__restart/src/commonents/List.jsx
+++ onebite_react/section08__restart/src/commonents/List.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import './List.css';
-import TodoItem from './TodoItem';
+import { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
 
-const List = ({ todos, onUpdate }) => {
-  const [search, setSearch] = useState('');
+const List = ({ todos, onUpdate, onUpdateAll, isAllCompleted, onDelete }) => {
+  const [search, setSearch] = useState("");
 
-  const onchangeSearch = (e) => {
+  const onchangeSearch = e => {
     // 매개변수로 setSearch 를 받아와서 전달한다.
     setSearch(e.target.value);
   };
 
   //<검색기능
   const getFilteredData = () => {
-    if (search === '') {
+    if (search === "") {
       return todos;
     }
     // includes 는
@@ -20,7 +20,7 @@ const List = ({ todos, onUpdate }) => {
     // true/false 로 반환한다.
     // 현재 todo의 컨텐츠에서 search 값을 찾는것.
     // (todo) => todo.content.includes(search);
-    return todos.filter((todo) => todo.content.toLowerCase().includes(search));
+    return todos.filter(todo => todo.content.toLowerCase().includes(search));
   };
 
   const filteredTodos = getFilteredData();
@@ -36,11 +36,29 @@ const List = ({ todos, onUpdate }) => {
         placeholder="검색어를 입력하세요"
       />
       <div className="Todos_wrapper">
+        <div>
+          <label>
+            <input
+              checked={isAllCompleted} // 모든 항목이 선택되었을 때만 체크됨
+              onChange={onUpdateAll}
+              type="checkbox"
+            />
+            전체선택
+          </label>
+        </div>
         {/* todos 의 데이터를 받아와서 map 로 뿌려준다  */}
-        {filteredTodos.map((todo) => {
+        {filteredTodos.map(todo => {
           // return <div>{todo.content}</div>;
           // 모든 리스트에는 key값이 반드시 필요하다. key={todo.id}
-          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
+          return (
+            <TodoItem
+              onChange={onchangeSearch}
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
         })}
       </div>
     </div>
